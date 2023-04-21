bc5b6ea9821f8e4311c6332b32ef2508707e6233
Gio <gohan1703@gmail.com>
2023-04-20 22:17:51 -0400

    First

---
+++ public/server.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+
+var PORT = process.env.PORT || 3000;
+
+var app = express();
+app.set('view engine', 'ejs');
+
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json({ type: '*/*' }));
+
+var router = express.Router();
+
+router.use(function (request, response, next) {
+    console.log("REQUEST:" + request.method + "   " + request.url);
+    console.log("BODY:" + JSON.stringify(request.body));
+    response.setHeader('Access-Control-Allow-Origin', '*');
+    response.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    response.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    response.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
+
+
+app.get('/', (req, res) => {
+    console.log("REFRESHED!");
+    res.render('index'); //Renders EJS 
+});
+
+app.use(express.static('public'));
+app.use('/', router);
+app.listen(PORT);

585f708cddf75023a333c5fa20da82154585ecd0
Gio <44237862+Redifexx@users.noreply.github.com>
2023-04-27 11:32:17 -0400

    Revert "oracledb"
    This reverts commit 35702932eaa4fe5345fe5951ef0a381bade5f783.

--- public/logininfo.js
+++
@@ -1,2 +0,0 @@
-export const username = "gperezcolon";
-export const pw = "SFM7tMFx37EpcKrP5Utr6Y2c";
--- public/server.js
+++ public/server.js
@@ -1,7 +1,5 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import oracledb from 'oracledb';
-import { username, pw } from './logininfo.js';
 import { hello } from './algorithms.js';
 
 var PORT = process.env.PORT || 3000;
@@ -9,20 +7,6 @@ var PORT = process.env.PORT || 3000;
 var app = express();
 app.set('view engine', 'ejs');
 
-var connectionProperties = 
-{
-    user: username,
-    password: pw,
-    connectionString: "(DESCRIPTION=(ADDRESS=(PROTOCOL=TCP)(HOST=oracle.cise.ufl.edu)(PORT=1521))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=orcl)))"
-}
-
-function doRelease(con) {
-    con.release(function (err) {
-      if (err) {
-        console.error(err.message);
-      }
-    });
-}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ type: '*/*' }));
@@ -39,6 +23,8 @@ router.use(function (request, response, next) {
     next();
 });
 
+
+
 app.get('/', (req, res) => {
     console.log("REFRESHED!");
     res.render('index'); //Renders EJS 
@@ -49,30 +35,6 @@ app.post('/test', (req, res) => {
     var test = req.body.query; //HTML to JS
     res.send(toSend) //JS TO HTML
 });
-
-app.post('/query1', (req, res) => {
-    const query = req.body.query;
-    console.log('Query received from client:', query);
-    oracledb.getConnection(connectionProperties, function (err, con) {
-        if (err) {
-          console.error(err.message);
-          res.status(500).send("Error connecting to DB");
-          return;
-        }
-        con.execute(query,{},  
-        { outFormat: oracledb.OBJECT },
-        function (err, result) {
-            if (err) {
-                console.error(err.message);
-                res.status(500).send("Error getting data from DB");
-                doRelease(con);
-                return;
-            }
-            console.log(result);
-            res.status(200).send(result);
-        });
-      });
-  });
 
 console.log(hello);
 
